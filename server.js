2d3b6409104ba84db2b35aab75db58d54bcf6ccc
Sougata Bauri <90753996+RahulIsBestCoder@users.noreply.github.com>
2023-02-02 14:16:25 +0530

    dotenv and htttp added to package

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose');
 const bodyParser=require('body-parser')
 require("dotenv").config();
 const database = mongoose.set('strictQuery', true);
+console.log(new Date());
 mongoose.connect(process.env.DB);
 
 mongoose.connection.on('error',err=>{
